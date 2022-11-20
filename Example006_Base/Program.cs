81d22fcd212db9a15e297f9e7e09aac9f82eb314
Aleksei Svitoch <pndlm@yandex.ru>
2022-11-20 18:10:14 +0600

    add Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 1;
+int numberB = 8;
+int numberC = 3;
+int numberD = 2;
+int numberE = 6;
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+if (numberE > max) max = numberE;
+
+Console.Write("max = ");
+Console.WriteLine(max);
